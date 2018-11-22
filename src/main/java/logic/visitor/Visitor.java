565368acb874fbf42de7f6c5ee52152c822c57e8
joseastorgat <jose.n.astorga.tobar@gmail.com>
2018-11-22 19:37:40 -0300

    fix visitor add level and empty level

--- src/main/java/logic/visitor/Visitor.java
+++ src/main/java/logic/visitor/Visitor.java
@@ -1,4 +1,55 @@
 package logic.visitor;
 
-public class Visitor {
+import logic.brick.GlassBrick;
+import logic.brick.MetalBrick;
+import logic.brick.WoodenBrick;
+import logic.level.EmptyLevel;
+import logic.level.PlayableLevel;
+
+import java.awt.dnd.DropTarget;
+
+/**
+ * Class that represents a Visitor object
+ *
+ * @author José Astorga
+ */
+public abstract class Visitor {
+
+    /**
+     * Visits the bricks of a Level
+     *
+     * @param level PlayableLevel to visit
+     */
+    public void visitPlayableLevel(PlayableLevel level) {}
+
+
+    /**
+     * Visits the bricks of a Level
+     *
+     * @param level EmptyLevel to visit
+     */
+    public void visitEmptyLevel(EmptyLevel level) {}
+
+
+    /**
+     * Visits a GlassBrick
+     *
+     * @param glassBrick GlassBrick to visit
+     */
+    public void visitGlassBrick(GlassBrick glassBrick) { }
+
+    /**
+     * Visits a WoodenBrick
+     *
+     * @param woodenBrick WoodenBrick to visit
+     */
+    public void visitWoodenBrick(WoodenBrick woodenBrick) { }
+
+    /**
+     * Visits a MetalBrick
+     *
+     * @param metalBrick MetalBrick to visit
+     */
+    public void visitMetalBrick(MetalBrick metalBrick) { }
+
 }
