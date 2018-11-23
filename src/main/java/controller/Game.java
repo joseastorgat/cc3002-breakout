efcc7926fe33ac5dcc90e59f970edd747785782c
joseastorgat <jose.n.astorga.tobar@gmail.com>
2018-11-23 18:07:09 -0300

    Game and HTF init

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -1,14 +1,28 @@
 package controller;
 
+import logic.brick.Brick;
+import logic.level.EmptyLevel;
+import logic.level.Level;
+
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  * Game logic controller class.
  *
  * @author Juan-Pablo Silva
+ * @author José Astorga
  */
-public class Game {
+public class Game implements Observer {
+    private Level level;
+    private int balls;
+    private int points;
 
     public Game(int balls) {
-
+        this.level = new EmptyLevel();
+        this.balls = balls;
+        this.points = 0;
     }
 
     /**
@@ -20,5 +34,65 @@ public class Game {
      */
     public boolean winner() {
         return false;
+    }
+
+    public void goNextLevel() {
+    }
+
+    public boolean isGameOver() {
+        return balls==0 || this.winner();
+    }
+
+    public void dropBall() {
+        if(balls>0)
+            balls-=1;
+    }
+
+    public int getBalls() {
+        return balls;
+    }
+
+    public int getCurrentPoints() {
+        return points;
+    }
+
+    public Level getCurrentLevel() {
+        return level;
+    }
+
+    public List<Brick> getBricks() {
+        return level.getBricks();
+    }
+
+    public boolean hasNextLevel() {
+        return level.hasNextLevel();
+    }
+
+    public int numberOfBricks() {
+        return this.level.getNumberOfBricks();
+    }
+
+    public boolean hasCurrentLevel() {
+        return level.isPlayableLevel();
+    }
+
+    public String getLevelName() {
+        return level.getName();
+    }
+
+    public void setCurrentLevel(Level level) {
+        this.level = level;
+    }
+
+    public void addPlayingLevel(Level level) {
+        this.level.addPlayingLevel(level);
+    }
+
+    public int getLevelPoints() {
+        return level.getPoints();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
     }
 }
--- src/main/java/facade/HomeworkTwoFacade.java
+++ src/main/java/facade/HomeworkTwoFacade.java
@@ -3,6 +3,7 @@ package facade;
 import controller.Game;
 import logic.brick.Brick;
 import logic.level.Level;
+import logic.level.PlayableLevel;
 
 import java.util.List;
 
@@ -31,7 +32,7 @@ public class HomeworkTwoFacade {
      * @see Level
      */
     public Level newLevelWithBricksFull(String name, int numberOfBricks, double probOfGlass, double probOfMetal, int seed) {
-        return null;
+        return new PlayableLevel(name, numberOfBricks, probOfGlass, probOfMetal, seed);
     }
 
     /**
@@ -45,7 +46,7 @@ public class HomeworkTwoFacade {
      * @see Level
      */
     public Level newLevelWithBricksNoMetal(String name, int numberOfBricks, double probOfGlass, int seed) {
-        return null;
+        return new PlayableLevel(name, numberOfBricks, probOfGlass, 0, seed);
     }
 
     /**
@@ -54,7 +55,7 @@ public class HomeworkTwoFacade {
      * @return the number of intact bricks in the current level
      */
     public int numberOfBricks() {
-        return 0;
+        return game.numberOfBricks();
     }
 
     /**
@@ -63,7 +64,7 @@ public class HomeworkTwoFacade {
      * @return the list of bricks
      */
     public List<Brick> getBricks() {
-        return null;
+        return game.getBricks();
     }
 
     /**
@@ -72,13 +73,14 @@ public class HomeworkTwoFacade {
      * @return true if the current level's next level is playable, false otherwise
      */
     public boolean hasNextLevel() {
-        return false;
+        return game.hasNextLevel();
     }
 
     /**
      * Pass to the next level of the current {@link Level}. Ignores all conditions and skip to the next level.
      */
     public void goNextLevel() {
+        game.goNextLevel();
 
     }
 
@@ -88,7 +90,7 @@ public class HomeworkTwoFacade {
      * @return true if the current level is playable, false otherwise
      */
     public boolean hasCurrentLevel() {
-        return false;
+        return game.hasCurrentLevel();
     }
 
     /**
@@ -97,7 +99,7 @@ public class HomeworkTwoFacade {
      * @return the name of the current level
      */
     public String getLevelName() {
-        return null;
+        return game.getLevelName();
     }
 
     /**
@@ -107,7 +109,7 @@ public class HomeworkTwoFacade {
      * @see Level
      */
     public Level getCurrentLevel() {
-        return null;
+        return game.getCurrentLevel();
     }
 
     /**
@@ -117,6 +119,7 @@ public class HomeworkTwoFacade {
      * @see Level
      */
     public void setCurrentLevel(Level level) {
+        game.setCurrentLevel(level);
 
     }
 
@@ -126,6 +129,7 @@ public class HomeworkTwoFacade {
      * @param level the level to be added
      */
     public void addPlayingLevel(Level level) {
+        game.addPlayingLevel(level);
 
     }
 
@@ -135,7 +139,7 @@ public class HomeworkTwoFacade {
      * @return the number of points in the current level
      */
     public int getLevelPoints() {
-        return 0;
+        return game.getLevelPoints();
     }
 
     /**
@@ -144,7 +148,7 @@ public class HomeworkTwoFacade {
      * @return the cumulative points
      */
     public int getCurrentPoints() {
-        return 0;
+        return game.getCurrentPoints();
     }
 
     /**
@@ -153,7 +157,7 @@ public class HomeworkTwoFacade {
      * @return the number of available balls
      */
     public int getBallsLeft() {
-        return 0;
+        return game.getBalls();
     }
 
     /**
@@ -162,7 +166,8 @@ public class HomeworkTwoFacade {
      * @return the new number of available balls
      */
     public int dropBall() {
-        return 0;
+        game.dropBall();
+        return game.getBalls();
     }
 
     /**
@@ -171,7 +176,7 @@ public class HomeworkTwoFacade {
      * @return true if the game is over, false otherwise
      */
     public boolean isGameOver() {
-        return false;
+        return game.isGameOver();
     }
 
     /**
